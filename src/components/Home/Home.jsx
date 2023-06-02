d3c39be76a9e8129be7bf0ee67973a9c0a5bc266
GlueFrontDevA1 <majd_abed@gluetron.com>
2023-06-02 08:47:27 +0300

    fffbf

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -84,8 +84,8 @@ const Home = () => {
           homeData.map((element) => {
             console.log(element, element.is_live);
             if (element.is_live === "1") {
+              console.log(element, element.category_name, categoryName);
               if (categoryName !== "all") {
-                console.log(element, element.category_name, categoryName);
                 if (element.category_name === categoryName) {
                   if (isCategoryEmpty) {
                     setIsCategoryEmpty(false);
