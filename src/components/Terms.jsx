beda7f93da4bd30e7b2f0682e4f150af3b37b07b
GlueFrontDevA1 <majd_abed@gluetron.com>
2023-06-17 11:28:29 +0300

    fnekjsnfkjn

--- src/components/Terms.jsx
+++ src/components/Terms.jsx
@@ -3,26 +3,26 @@ import { Link } from "react-router-dom";
 const Terms = () => {
   return (
     <div className='terms-body'>
-      <div class='container'>
-        <div class='wow-logo'>
+      <div className='container'>
+        <div className='wow-logo'>
           <Link to='/'>
             <img
-              class='img-responsive terms-img-logo'
+              className='img-responsive terms-img-logo'
               src={require("../assets/images/wow_logo.png")}
             />
           </Link>
         </div>
-        <section class='generic text-center'>
+        <section className='generic text-center'>
           <h2 className='terms-header'>Terms and Conditions</h2>
-          <hr class='hr-mid' />
+          <hr className='hr-mid' />
           <img
-            class='img-generic img-responsive'
+            className='img-generic img-responsive'
             style={{ borderRadius: "2rem" }}
             src={require("../assets/images/terms.jpeg")}
           />
         </section>
-        <section class='font-2'>
-          <p class='generic-p'>
+        <section className='font-2'>
+          <p className='generic-p'>
             Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in
             faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis.
             Praesent rutrum sem diam, vitae egestas enim auctor sit amet.
@@ -32,7 +32,7 @@ const Terms = () => {
             commodo eget turpis at, elementum convallis elit. Pellentesque enim
             turpis, hendrerit tristique.
           </p>
-          <p class='generic-p'>
+          <p className='generic-p'>
             Interdum et malesuada fames ac ante ipsum primis in faucibus.
             Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent
             rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo
@@ -40,7 +40,7 @@ const Terms = () => {
             turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit
             tristique lorem ipsum dolor.
           </p>
-          <p class='generic-p'>
+          <p className='generic-p'>
             Interdum et malesuada fames ac ante ipsum primis in faucibus.
             Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent
             rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo
@@ -57,40 +57,40 @@ const Terms = () => {
         </section>
       </div>
 
-      <section class='social'>
+      <section className='social'>
         <ul>
           <li>
-            <a href='#' class='fa fa-facebook fa-2x' aria-hidden='true'></a>
+            <a href='#' className='fa fa-facebook fa-2x' aria-hidden='true'></a>
           </li>
           <li>
-            <a href='#' class='fa fa-twitter fa-2x' aria-hidden='true'></a>
+            <a href='#' className='fa fa-twitter fa-2x' aria-hidden='true'></a>
           </li>
           <li>
-            <a href='#' class='fa fa-linkedin fa-2x' aria-hidden='true'></a>
+            <a href='#' className='fa fa-linkedin fa-2x' aria-hidden='true'></a>
           </li>
           <li>
-            <a href='#' class='fa fa-youtube fa-2x' aria-hidden='true'></a>
+            <a href='#' className='fa fa-youtube fa-2x' aria-hidden='true'></a>
           </li>
         </ul>
       </section>
 
-      <section class='footer'>
-        <div class='container'>
-          <div class='row'>
-            <div class='col-lg-5 col-md-5 col-sm-5 col-xs-5'>
-              <div class='footer-p1'>
-                <p class='font-2'>Copy right 2023 by Oly Oli</p>
+      <section className='footer'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-5 col-md-5 col-sm-5 col-xs-5'>
+              <div className='footer-p1'>
+                <p className='font-2'>Copy right 2023 by Oly Oli</p>
               </div>
             </div>
-            <div class='col-lg-2 col-md-2 col-sm-2 col-xs-2'>
+            <div className='col-lg-2 col-md-2 col-sm-2 col-xs-2'>
               <img
-                class='img-responsive foot-img'
+                className='img-responsive foot-img'
                 src={require("../assets/images/footer-logo.png")}
               />
             </div>
-            <div class='col-lg-5 col-md-5 col-sm-5 col-xs-5'>
-              <div class='footer-p2'>
-                <p class='font-2'>
+            <div className='col-lg-5 col-md-5 col-sm-5 col-xs-5'>
+              <div className='footer-p2'>
+                <p className='font-2'>
                   <Link to='/privacy'>Privacy</Link> &<Link to='/terms'>Terms</Link>
                 </p>
               </div>
