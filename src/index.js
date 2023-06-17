1f43aabca1b880ccdc6ac2ada867dcf1e332ef2a
GlueFrontDevA1 <majd_abed@gluetron.com>
2023-06-17 10:49:04 +0300

    csdcds

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { ContextProvider } from './context';
 import './css/bootstrap.css';
-import "./css/font-awesome/css/font-awesome.min.css"
+// import "./css/font-awesome/css/font-awesome.min.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
