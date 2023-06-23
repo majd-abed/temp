f8076615cbb430375314c53457f19663ad435ce4
GlueFrontDevA1 <majd_abed@gluetron.com>
2023-06-23 15:03:36 +0300

    dddddddddds

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
       )
       .then((res) => {
         if (res.status === 201) {
-          console.log(res.data);
+          console.log(res.data.videos);
           setHomeData(res.data.videos);
           setIsHomeLoading(false);
           setIsSearch(true);
