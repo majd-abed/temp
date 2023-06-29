a22dc62fba0f5a46e84998ac5cae736bb9ae285a
GlueFrontDevA1 <majd_abed@gluetron.com>
2023-06-29 15:01:18 +0300

    adsfadsfads

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -534,7 +534,7 @@ const Profile = () => {
                       }}
                     />
                     <span
-                      className='password-show'
+                      className='password-show-profile'
                       onClick={() => setIsShowEmailPass(!isShowEmailPass)}>
                       {isShowEmailPass ? (
                         <span class='material-symbols-outlined'>visibility_off</span>
@@ -567,19 +567,30 @@ const Profile = () => {
               )}
 
               <div className='password-container'>
-                <input
-                  ref={passwordRef}
-                  type='password'
-                  name='password'
-                  id='newPassword'
-                  className='password-input'
-                  placeholder='New Password'
-                  onChange={() => {
-                    setIsPasswordInvalid(false);
-                    // setIsCurrPasswordShort(false);
-                    // setIsPasswordShort(false);
-                  }}
-                />
+                <div style={{ position: "relative" }}>
+                  <input
+                    ref={passwordRef}
+                    type='password'
+                    name='password'
+                    id='newPassword'
+                    className='password-input'
+                    placeholder='New Password'
+                    onChange={() => {
+                      setIsPasswordInvalid(false);
+                      // setIsCurrPasswordShort(false);
+                      // setIsPasswordShort(false);
+                    }}
+                  />
+                  <span
+                    className='password-show-profile'
+                    onClick={() => setIsShowPass(!isShowPass)}>
+                    {isShowPass ? (
+                      <span class='material-symbols-outlined'>visibility_off</span>
+                    ) : (
+                      <span class='material-symbols-outlined'>visibility</span>
+                    )}
+                  </span>
+                </div>
                 {isPasswordInvalid ? (
                   <div
                     className='require-sign'
@@ -602,18 +613,31 @@ const Profile = () => {
                       className='change-btn'>
                       Cancel
                     </button>
-                    <input
-                      ref={currPasswordRef}
-                      type='password'
-                      name='password'
-                      id='currpassword'
-                      className='password-input extra-margin'
-                      placeholder='Current Password'
-                      onChange={() => {
-                        // setIsCurrPasswordShort(false);
-                        // setIsPasswordShort(false);
-                      }}
-                    />
+                    <div style={{ position: "relative" }}>
+                      <input
+                        ref={currPasswordRef}
+                        type='password'
+                        name='password'
+                        id='currpassword'
+                        className='password-input extra-margin'
+                        placeholder='Current Password'
+                        onChange={() => {
+                          // setIsCurrPasswordShort(false);
+                          // setIsPasswordShort(false);
+                        }}
+                      />
+                      <span
+                        className='password-show-profile'
+                        onClick={() => setIsShowCurrPass(!isShowCurrPass)}>
+                        {isShowCurrPass ? (
+                          <span class='material-symbols-outlined'>
+                            visibility_off
+                          </span>
+                        ) : (
+                          <span class='material-symbols-outlined'>visibility</span>
+                        )}
+                      </span>
+                    </div>
                   </>
                 ) : null}
               </div>
